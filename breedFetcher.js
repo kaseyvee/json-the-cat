fbaa66e2cb3876dc9ccadfe2dc383d2e2ba5d689
kethnal <kaseyvaldez@hotmail.com>
2022-09-29 14:36:10 -0700

    make mocha and chai test for breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,15 @@
 const request = require('request');
 
-const arg = process.argv.slice(2);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length === 0) return callback(null, "Uh-oh! We can't find this breed!");
+      if (data.length !== 0) return callback(null, data[0]["description"]);
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${arg[0]}`, (error, response, body) => {
-  if (error) {
-    console.log("Uh-oh! Error!", error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) console.log("Uh-oh! We can't find this breed!");
-    if (data.length !== 0) console.log(data[0]["description"]);
-  }
-});
+module.exports = { fetchBreedDescription };
